cfe7caef74178a699db97f5c507fd3d1b513a59a
pointgenesis <travis.steinmetz@yaho.com>
2019-10-16 00:12:48 -0400

    created doubly-linked list implementation

--- LinkedList/node.py
+++ LinkedList/node.py
@@ -1,4 +1,7 @@
 class Node(object):
+    """
+    Node object for a singly-linked list that contains only references to the next node. There is no back pointer.
+    """
     def __init__(self, data):
         self.data = data
         self.next_node = None
